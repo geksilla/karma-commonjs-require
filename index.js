ffd6f2c70b1b9ad01925bcff7474b84f985b9a57
geksilla <buzhordenis@gmail.com>
2014-11-25 15:37:40 +0200

    Fix wrong file extension replacing.

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var createCommonRequirePreprocessor = function(args, logger, config, basePath) {
   var appDir = config.appDir || 'app';
   return function(content, file, done) {
     var processed = null;
-    var modulePath = file.path.replace(basePath, '').replace(new RegExp("\/" + appDir + "\/"), "").replace(/^\//, '').replace(/\..+$/, '');
+    var modulePath = file.path.replace(basePath, '').replace(new RegExp("\/" + appDir + "\/"), "").replace(/^\//, '').replace(path.extname(file.path), '');
     log.debug('Processing %s register module "%s"', file.path, modulePath);
     try {
       processed = ";require.register(\""+ modulePath +"\", function(exports, require, module){\n" + content + "\n})";
